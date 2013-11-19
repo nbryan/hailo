365c78b77cbf7f111c00773a2ba13905a8ad9893
Nathan Bryan <nathan.bryan@gmail.com>
2013-11-18 23:58:53 -0500

    Robots move

--- main.go
+++ main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+    "encoding/csv"
+    "os"
     "fmt"
+    "strconv"
+    "time"
 )
 
-func main() {
-    tubeStations := LoadTubeStations()
+type Instruction struct {
+    Location LatLong
+    Time time.Time
+}
 
-    for _, station := range tubeStations {
-        fmt.Println(station)
+var tubeStations []TubeStation // Evil global variable
+
+func main() {
+    tubeStations = LoadTubeStations()
+
+    ids := []int{5937, 6043}
+    done := make(chan int)
+
+    for _, id := range ids {
+        go Dispatch(Robot{id, LatLong{0, 0}}, done)
     }
+
+    for i := len(ids); i > 0; {
+        <-done
+        i--
+    }
+}
+
+func Dispatch(r Robot, done chan int) {
+    fmt.Println("Dispatch")
+    ch := make(chan Instruction, 10) // Only allow 10 instructions in queue at a time
+    go ReadInstructions(r.ID, ch)
+
+    for instruction := range ch {
+        r.ReceiveInstruction(instruction)
+        fmt.Println(r.ID, instruction)
+    }
+
+    done <- 1
+}
+
+func ReadInstructions(id int, ch chan Instruction) {
+    fmt.Println("ReadInstructions")
+    file, _ := os.Open(fmt.Sprintf("%v.csv", id))
+    defer file.Close()
+
+    reader := csv.NewReader(file)
+    for {
+        record, err := reader.Read()
+        if err != nil {
+            break
+        }
+
+        lat, _ := strconv.ParseFloat(record[1], 64)
+        long, _ := strconv.ParseFloat(record[2], 64)
+        time, _ := time.Parse("2006-01-02 15:04:05", record[3])
+        instruction := Instruction{LatLong{lat, long}, time}
+
+        ch <- instruction
+    }
+
+    close(ch)
 }
--- robot.go
+++ robot.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+    "time"
+    "fmt"
+)
+
 type Robot struct{
     ID int
+    Location LatLong
+}
+
+func (r *Robot) ReceiveInstruction(instruction Instruction) {
+    r.Move(instruction.Location)
+}
+
+const speed int64 = 100000 // Meters per hour (100kph)
+
+func (r *Robot) Move(newLocation LatLong) {
+    startingLocation := LatLong{0, 0}
+    if r.Location != startingLocation { // Assume it takes no time to move to starting location
+        time.Sleep(time.Duration(int64(r.Location.DistanceFrom(newLocation)) * int64(time.Hour) / speed))
+    }
+    r.Location = newLocation
+}
+
+func (r *Robot) IsNearTheTube(tubeStations []TubeStation) bool {
+    return true
 }
